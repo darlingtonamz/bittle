4125b24b8b48e3b32c578c98ff65a69b47b387ed
Edwin Tsatsu <paynegreen@yahoo.com>
2016-03-18 07:27:16 +0000

    UI integration for businesses and users

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :users do
     member do
       resources :payments
-      resources :businesses do
-        resources :invoices
-      end
     end
   end
 
+  resources :businesses do
+    member do
+      resources :invoices
+    end
+  end
 end
